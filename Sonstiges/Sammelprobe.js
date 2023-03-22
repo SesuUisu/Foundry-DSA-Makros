37495c14f8ddd329e86e09289759fe46eb7be2c8
Moritz <moritz.huck@rwth-aachen.de>
2023-03-22 11:30:26 +0100

    verbesserungen Output

--- Sonstiges/Sammelprobe.js
+++ Sonstiges/Sammelprobe.js
@@ -24,7 +24,7 @@ async function main() {
                  <label for="talent_choice">Talent: </label>
                  <select name="talent_choice" id="talent_choice" style="float:right">`;
     for (const talent of talents) {
-        talent_dialog += "<option value=" + talent._id + ">" + talent.name + "</option>";
+        talent_dialog += `<option value=${talent._id}> ${talent.name}</option>`;
     }
 
     talent_dialog += `</select></form>` + divFlexEnd;
@@ -60,26 +60,98 @@ async function main() {
         const maxroll = Number(html.find("#maxroll")[0].value);
         let accum_tap = 0;
         let number_rolls = 0;
-        let number_crit_suc =0;
-        let number_crit_fail =0;
+        let number_crit_suc = 0;
+        let number_crit_fail = 0;
+        let results = [];
         while (number_rolls < maxroll && accum_tap < target) {
             const result = await rollSkill(item[0], mod);
             number_rolls += 1;
-            showRollToChat(result);
+            //showRollToChat(result);
             if (checkOccurence(result, 20)) {
                 result.success = false;
-                number_crit_fail +=1;
+                number_crit_fail += 1;
             } else if (checkOccurence(result, 1)) {
                 result.success = true;
                 accum_tap += Math.max(Number(item[0].system.value), 1);
+                result.roll.total = Math.max(Number(item[0].system.value), 1);
                 number_crit_suc += 1;
             } else if (result.success) {
                 accum_tap += Math.max(Number(result.roll.total), 1);
             }
+            results.push(result);
+
         }
 
-    }
 
+        let chatData = {
+            user: game.user._id,
+            speaker: ChatMessage.getSpeaker(),
+            flavor: `Sammelprobe ${item[0].name} +${mod} (${item[0].system.value}) [${target} TaP* / ${maxroll}]`,
+            content: createMeassgeContent(results, accum_tap, number_rolls, number_crit_suc, number_crit_fail)
+        };
+        ChatMessage.create(chatData, {});
+
+    }
+}
+
+function createMeassgeContent(results, accum_tap, number_rolls, number_crit_suc, number_crit_fail) {
+    const content = results.reduce((acc, result) => {
+        let section = result.roll.dice.reduce((acc, dice) => {
+            let rollDiceParts = `${dice.number}d${dice.faces}`;
+            let diceTotal = dice.results.reduce((acc, die) => {
+                if (!die.discarded) acc += die.result;
+                return acc;
+            }, 0);
+            let diceList = dice.results.reduce((total, e) => {
+                let discarded = e.discarded ? "discarded" : "";
+                let exploded = e.exploded ? "exploded" : "";
+                let critFail = dice.faces === e.result ? "max" : e.result === 1 ? "min" : "";
+                total += `<li class="roll die ${discarded} ${exploded} d${dice.faces} ${critFail}">${e.result}</li>`;
+                return total;
+            }, ``);
+            acc += `<section class="tooltip-part">
+                        <div class="dice">
+                            <header class="part-header flexrow">
+                                <span class="part-formula">${rollDiceParts}</span>
+                                <span class="part-total">${diceTotal}</span>
+                            </header>
+                            <ol class="dice-rolls">
+                                ${diceList}
+                            </ol>
+                        </div>
+                    </section>`;
+
+            return acc;
+        }, ``);
+        acc += `
+            <div class="dice-roll">
+                <div class="dice-result">
+                   <div class="flexrow">`;
+        const c = result.roll.dice.map(function (e, i) {
+            return [e, result.options.testAttributeData[i]];
+        });
+
+        for (const d of c) {
+            acc += `<div class="dice-total roll-row">
+                <div class="roll ">${d[0].total}</div>
+                <div class="roll-info">${d[1].name} (${d[1].value})</div>
+            </div>`;
+        }
+        //<div class="dice-formula">${rollFormula}</div>;
+        acc += `</div> 
+                <div class="dice-tooltip" style="display:none;">
+                        ${section}
+                    </div>
+                    <h4 class="dice-total">${result.roll.total}</h4>
+                </div>
+            </div>
+        `;
+        return acc;
+    }, '');
+    for (let result of results) {
+        game.dice3d?.showForRoll(result.roll);
+    }
+    return content;
 }
 
 function checkOccurence(result, target) {
@@ -90,7 +162,6 @@ function checkOccurence(result, target) {
 }
 
 function showRollToChat(result) {
-
     const rollSkillToChat = game.ruleset.effects.get("rollSkillToChat");
     rollSkillToChat.apply(result);
 }
